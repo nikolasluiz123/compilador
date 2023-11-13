bcca849cc2453b6561027c8adda7ab9fd81a872a
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2023-11-13 16:25:59 -0300

    refactor: alterando para um projeto maven para possibilitar gerar jar facilmente

--- JavaFXCompiler/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module JavaFXCompiler {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	
-	opens br.com.java.compiler.controller to javafx.graphics, javafx.fxml;
-	opens br.com.java.compiler to javafx.graphics, javafx.fxml;
-}
